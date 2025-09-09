32395aed22905de20e998e66a8321e7d26fe2a8f
Luntu Mavukuza <luntu.mavukz35@gmail.com>
2025-09-09 20:32:13 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Luntu Mavukuza on 2025/09/09.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
